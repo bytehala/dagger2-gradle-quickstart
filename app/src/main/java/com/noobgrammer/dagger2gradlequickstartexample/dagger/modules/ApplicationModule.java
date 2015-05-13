9d0b1193dccb463308700b66fb689511f1cca2b0
Lem Dulfo <lem.dulfo@samsung.com>
2015-05-13 17:07:03 +0800

    Step 2: Add our module. Our ApplicationModule will be able to inject the ApplicationContext to classes that need it.

---
+++ app/src/main/java/com/noobgrammer/dagger2gradlequickstartexample/dagger/modules/ApplicationModule.java
@@ -0,0 +1,29 @@
+package com.noobgrammer.dagger2gradlequickstartexample.dagger.modules;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Lem on 5/13/2015.
+ */
+@Module
+public class ApplicationModule {
+
+    private final Application application;
+
+    public ApplicationModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    Context provideApplicationContext() {
+        return application.getApplicationContext();
+    }
+
+}
