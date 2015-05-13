c67762af525e7b8602229262b1a3566b4ee5e591
Lem Dulfo <lem.dulfo@samsung.com>
2015-05-13 15:16:30 +0800

    Add a lib dependency. This could be any module, in your project or from mavenCentral.

---
+++ pretend3rdpartylib/src/main/java/com/example/SomeThirdPartyClass.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class SomeThirdPartyClass {
+
+    public static int[] sort(int[] input) {
+        for (int i = 1; i < input.length; i++) {
+            int next = input[i];
+            // find the insertion location while moving all larger element up
+            int j = i;
+            while (j > 0 && input[j - 1] > next) {
+                input[j] = input[j - 1];
+                j--;
+            }
+            // insert the element
+            input[j] = next;
+        }
+        return input;
+    }
+
+}
