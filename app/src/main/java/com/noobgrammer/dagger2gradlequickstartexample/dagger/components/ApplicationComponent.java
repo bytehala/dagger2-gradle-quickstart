3584969f5bad9b449ccc395752189d1d353a5b4d
Lem Dulfo <lem.dulfo@samsung.com>
2015-05-13 17:54:55 +0800

    Step 5: Add inject methods to our Component interface. You need to add one inject() for every class that wants to participate in dependency injection. Notes about Dagger 2: Injecting into a superclass will not inject dependencies for the subclass. Injecting into the subclass apparently injects into the superclass.
    Notice that I injected OutputActivity. If I instead put BaseActivity here, and called inject() with an OutputActivity instance, the function call will work but all the injected fields will be null. That means, it looks good at compile time but fails at runtime.

--- app/src/main/java/com/noobgrammer/dagger2gradlequickstartexample/dagger/components/ApplicationComponent.java
+++ app/src/main/java/com/noobgrammer/dagger2gradlequickstartexample/dagger/components/ApplicationComponent.java
@@ -1,6 +1,6 @@
 package com.noobgrammer.dagger2gradlequickstartexample.dagger.components;
 
-import com.noobgrammer.dagger2gradlequickstartexample.InputActivity;
+import com.noobgrammer.dagger2gradlequickstartexample.OutputActivity;
 import com.noobgrammer.dagger2gradlequickstartexample.dagger.modules.ApplicationModule;
 
 import javax.inject.Singleton;
@@ -13,4 +13,16 @@ import dagger.Component;
 @Singleton
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
+    void inject(OutputActivity activity);
+    /**
+     * The following are also possible:
+     *
+     * void inject(InputActivity activity);
+     * void inject(BaseActivity activity);
+     * void inject(SomeFragmentSubclass fragment);
+     *
+     * They can all live harmoniously here, because they have different method signatures.
+     * I've also seen injectActivity() and injectFragment() variants. I have not done research into
+     * the history of this.
+     */
 }
